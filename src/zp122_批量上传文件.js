2a818963ab84b99dd69af7d3e3f3c71442d4518e
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-06-11 09:14:15 +0800

    1

--- src/zp122_批量上传文件.js
+++ src/zp122_批量上传文件.js
@@ -2,27 +2,29 @@ import React from "react"
 import css from "./zp122_批量上传文件.css"
 
 function render(ref) {
-    const { exc, render, props } = ref
-    if (!props.dbf) return <div>请配置表单字段</div>
-    let uploaded = ref.getForm(props.dbf)
-    if (!Array.isArray(uploaded)) {
-        if (uploaded) warn("表单字段必须是数组")
-        uploaded = []
+    const { dbf, form } = ref.props
+    let arr
+    if (form) {
+        ref.form = typeof form == "string" ? ref.excA(form) : form
+        if (typeof ref.form == "object") arr = ref.form[dbf]
+    } else if (ref.getForm) {
+        arr = ref.getForm(dbf)
     }
+    if (!Array.isArray(arr)) arr = []
+    ref.uploaded = arr
     return <React.Fragment>
         <input onChange={e => onChange(ref, e)} type="file" multiple="multiple"/>
-        <button onClick={e => ref.container.firstChild.click()} className="zbtn zellipsis">{EL.upload}&nbsp;{props.label || "上传文件"}</button>
-        {ref.uploading.map((a, i) => <div className={"zp122U zp122_" + i} key={i}>{EL.spin}{a}<i></i></div>)}
-        {uploaded.map((a, i) => <div className="zp122D" key={a + i}>
-            {EL.handle}<a href={a}>{a.split("/")[a.split("/").length - 1]}</a>
-            <span onClick={e => {e.stopPropagation(); exc('confirm("确定要删除吗？")', {}, () => {uploaded.splice(i, 1); ref.setForm(props.dbf, uploaded)})}}>{EL.del}</span>
+        {arr.map((a, i) => <div className="zp122D" key={a + i}>
+            <span className="zmove zhover"/><a href={a} target="_blank">{a.split("/")[a.split("/").length - 1]}</a>
+            <span onClick={e => {e.stopPropagation(); ref.exc('confirm("确定要删除吗？")', {}, () => {arr.splice(i, 1); ref.form ? ref.form[dbf] = arr : ref.setForm(dbf, arr); ref.exc('render()')})}} className="zdel zhover"/>
         </div>)}
+        {ref.uploading.map((a, i) => <div className={"zp122U zp122_" + i} key={i}><span className="zloading"/><i/>{a}</div>)}
+        <button onClick={e => ref.container.firstChild.click()} className="zbtn zellipsis">{Upload}<span>&nbsp;{ref.props.label || "上传文件"}</span></button>
     </React.Fragment>
 }
 
 function init(ref) {
-    const { getForm, id, exc, props, render } = ref
-    if (!getForm) return exc('warn("请置于表单容器中")')
+    const { id, exc, props, render } = ref
     ref.uploading = []
     exc('load("//z.zccdn.cn/vendor/Sortable_1.13.0.js")', {}, () => {
         new Sortable(ref.container, {
@@ -30,12 +32,10 @@ function init(ref) {
             forceFallback: true,
             fallbackTolerance: 5,
             onSort: e => {
-                let arr = getForm(props.dbf)
-                if (!Array.isArray(arr)) arr = []
-                arr.splice(e.newDraggableIndex, 0, arr.splice(e.oldDraggableIndex, 1)[0])
-                ref.setForm(props.dbf, arr)
+                ref.uploaded.splice(e.newDraggableIndex, 0, ref.uploaded.splice(e.oldDraggableIndex, 1)[0])
+                ref.form ? ref.form[props.dbf] = ref.uploaded : ref.setForm(props.dbf, ref.uploaded)
             },
-            handle: "#" + id + " .zp118handler",
+            handle: "#" + id + " .zmove",
             draggable: ".zp122D",
             dragClass: "zp122Drag",
             ghostClass: "zp122Drop"
@@ -47,6 +47,7 @@ function onChange(ref, e) {
     const { exc, render, props } = ref
     const arr = Array.from(e.target.files)
     if (!arr.length) return exc('warn("请选择文件")')
+    if (arr.find(a => a.size / 1048576 > (props.max || 5))) return exc(`warn("文件太大, 请压缩至${props.max || 5}M以下")`)
     arr.forEach((file, i) => setTimeout(() => {
         ref.uploading.push(file.name)
         render()
@@ -58,10 +59,12 @@ function onChange(ref, e) {
                 },
                 onSuccess: r => {
                     ref.uploading.splice(ref.uploading.indexOf(file.name), 1)
-                    let arr = ref.getForm(props.dbf)
-                    if (!Array.isArray(arr)) arr = []
-                    arr.unshift(r.url)
-                    ref.setForm(props.dbf, arr)
+                    ref.uploaded.unshift(r.url)
+                    ref.form ? ref.form[props.dbf] = ref.uploaded : ref.setForm(props.dbf, ref.uploaded)
+                    if (props.onSuccess) exc(props.onSuccess, { ...ref.ctx, $ext_ctx: ref.ctx, $val: ref.uploaded, ...r }, () => ref.exc("render()"))
+                    delete ref.progress
+                    delete ref.file
+                    ref.render()
                 },
                 onError: r => {
                     exc(`alert("上传出错了", r.error)`, { r })
@@ -77,20 +80,29 @@ $plugin({
     id: "zp122",
     props: [{
         prop: "dbf",
-        type: "text",
-        label: "表单字段"
+        label: "字段名",
+        ph: "必填"
+    }, {
+        prop: "form",
+        label: "字段容器",
+        ph: "如不填则使用祖先节点的表单容器"
+    }, {
+        prop: "max",
+        type: "number",
+        label: "最大文件大小(单位:MB)",
+        ph: "默认最大5MB"
     }, {
         prop: "label",
-        type: "text",
-        label: "[上传文件]文本"
+        label: "[上传文件] 文本"
+    }, {
+        prop: "onSuccess",
+        type: "exp",
+        label: "上传成功表达式",
+        ph: "$val"
     }],
     render,
     init,
     css
 })
 
-const EL = {
-    upload: <svg className="zsvg zp118upload" viewBox="64 64 896 896"><path d="M400 317.7h73.9V656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V317.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 163a8 8 0 0 0-12.6 0l-112 141.7c-4.1 5.3-.4 13 6.3 13zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"/></svg>,
-    handle: <svg className="zsvg zp118handler" viewBox="0 0 1024 1024"><path d="M512 512M190.272 248.512l643.52 0 0 74.944-643.52 0 0-74.944ZM190.272 474.496l643.52 0 0 74.944-643.52 0 0-74.944ZM190.272 700.544l643.52 0 0 74.944-643.52 0 0-74.944Z"/></svg>,
-    del: <svg className="zsvg zp118del" viewBox="64 64 896 896"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"/></svg>
-}
+const Upload = <svg className="zsvg" viewBox="64 64 896 896"><path d="M400 317.7h73.9V656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V317.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 163a8 8 0 0 0-12.6 0l-112 141.7c-4.1 5.3-.4 13 6.3 13zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"/></svg>
